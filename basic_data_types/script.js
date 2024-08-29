a367829ce84937f1d9cab3f1a2a55c4c1890b74a
jkung <jeremykung@gmail.com>
2024-08-29 15:44:05 +0300

    initial commit

---
+++ basic_data_types/script.js
@@ -0,0 +1,7 @@
+/* 
+1. Strings
+2. Numbers
+3. Boolean
+4. Null
+*/
+
---
+++ variables/script.js
@@ -0,0 +1,26 @@
+console.log("hello new world!")
+// Variables
+// variables are like containers that hold something
+// below the x container holds tea
+
+var x = "tea"   // defining variable
+x = "phone"     // mutating variable
+x = "anars"
+console.log(x)
+
+// 2018 convention changed from using "var" to "const/let"
+
+// const variables CANNOT change
+const y = "marker"
+// y = "mark"
+console.log(y)
+
+// let variables CAN change
+let z = "computer"
+console.log(z)
+
+// PRACTICE
+let mainCharacter = "Natalia"     // String variables
+let age = 12                    // Integer variables
+
+console.log(mainCharacter + " is " + age)
