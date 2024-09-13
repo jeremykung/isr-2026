5b147a7797bad669d885dd1f26c294e3a7cb127d
jkung <jeremykung@gmail.com>
2024-09-13 12:48:24 +0300

    emils story add

---
+++ variables/story/emils.js
@@ -0,0 +1,12 @@
+console.log( "anar story")
+
+let mainCharacter = "Anars"
+let teacher = "Mr. Kung"
+let school = "ISR"
+let sidecharacter = "Emils"
+
+console.log( " one day " + teacher + " gave us a homework to write a story " )
+console.log( sidecharacter + " didn't want to do it at all and forgot about it ")
+console.log( " everyday " + sidecharacter + " went to " + school + mainCharacter + " reminded him of the story ")
+console.log( " one day " + sidecharacter + " got tired of " + mainCharacter + " and decided to do the story " )
+console.log( " the story is done and now " + teacher + mainCharacter + " are happy, but " + sidecharacter + " is exhausted ")
