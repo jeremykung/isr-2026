b31207fe113b33a02e035e06c7262d018a0c15bd
jkung <jeremykung@gmail.com>
2025-02-10 15:13:50 +0200

    added forms

---
+++ forms/script.js
@@ -0,0 +1,6 @@
+function getFormData() {
+    const inputName = document.querySelector("#name").value
+    console.log("Input name is:", inputName)
+
+    // Modify this function so that it gets ALL the form data, and logs it
+}
