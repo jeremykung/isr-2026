19054b81e85a24d89a041b8b689b4ffae5bd6962
jkung <jeremykung@gmail.com>
2025-02-20 16:36:27 +0200

    updated supabase issue running local html with module scripts

---
+++ dom/functions.py
@@ -0,0 +1,18 @@
+def easy_function():
+  print("This is a function.")
+
+easy_function()
+
+
+def medium_function(an_example):
+  print(an_example + "Eugene")
+
+  easy_function("Gregory")
+  easy_function("Lambert")
+  easy_function("Floyd") 
+
+
+def hard_function(an_example, example_two):
+  print(an_example + " and " + example_two)
+
+hard_function("Gregory", "Eugene") 
--- dom/script.js
+++ dom/script.js
@@ -1,14 +1,30 @@
-console.log("DOM Manipulation!")
+const { createClient } = supabase;
 
-// Grab document objects and manipulate their text
-document.querySelector("h1").innerText = "Dogs"
+setTimeout(async () => {
 
-function showPicture() {
-
-    // Manipulate HTML
-    document.querySelector(".image-container").innerHTML = `
-        <p>this is a picture of a lizard</p>
-        <img src="https://i.ytimg.com/vi/CKlM82LJfTI/maxresdefault.jpg" />
-    `
+    const supabase = createClient('https://yadhdsukjpqsjpzytlyr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhZGhkc3VranBxc2pwenl0bHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyODA3NzgsImV4cCI6MjA1NDg1Njc3OH0.PyEW_wo76fCOkIYRR9v1prhMUi-TBugU79xIbXq66ow')
     
+    const { data, error } = await supabase
+        .from('user')
+        .select()
+
+    if (error) {
+    console.log('supabase error:', error)
+    } else {
+    console.log('data:', data)
+    }
+
+}, 1);
+
+export async function addData() {
+
+    const { error } = await supabase
+    .from('user')
+    .insert({ 
+        email: 'test@email.com', 
+        password: '123abc' ,
+        date: '2024-12-10'
+    })
+
+
 }
